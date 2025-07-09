856152dedcf3873dba5ac228e9ed7e955871fa9a
Karan Singh <karansingh25822@gmail.com>
2025-07-09 10:00:00 +0530

    Add DEFAULT_OUTPUT_TYPE and __all__ to config

--- config.py
+++ config.py
@@ -5,8 +5,12 @@ import os
 
 DEFAULT_MAX_LINES = 20
 DEFAULT_FORMAT = "text"
+DEFAULT_OUTPUT_TYPE = "generic"
 
 config = {
     "max_lines": int(os.environ.get("DISTILL_MAX_LINES", DEFAULT_MAX_LINES)),
     "format": os.environ.get("DISTILL_FORMAT", DEFAULT_FORMAT),
+    "output_type": os.environ.get("DISTILL_TYPE", DEFAULT_OUTPUT_TYPE),
 }
+
+__all__ = ["DEFAULT_MAX_LINES", "DEFAULT_FORMAT", "DEFAULT_OUTPUT_TYPE", "config"]
